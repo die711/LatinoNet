8aa1e4fb1dd3ff677c1d211824f52be483ad51e3
Diego Resendiz Ojeda <di_564@hotmail.com>
2023-11-03 20:59:55 -0700

    get all products

---
+++ LatinoNet.Controllers/GetAllProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LatinoNet.DTOs;
+using LatinoNet.Presenters;
+using LatinoNet.UseCasesPorts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LatinoNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllProductsController
+    {
+        private readonly IGetAllProductsInputPort _inputPort;
+        private readonly IGetAllProductsOutputPort _outputPort;
+
+        public GetAllProductsController(IGetAllProductsInputPort inputPort, IGetAllProductsOutputPort outputPort)
+        {
+            _inputPort = inputPort;
+            _outputPort = outputPort;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProductDto>> GetAllProducts()
+        {
+            await _inputPort.Handle();
+            return ((IPresenter<IEnumerable<ProductDto>>)_outputPort).Content;
+        }
+    }
+}
--- LatinoNet.IoC/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LatinoNet.IoC
-{
-    public class Class1
-    {
-    }
-}
